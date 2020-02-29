425ee0056874be9786fb6200925acaeb3da18924
Phil Hawksworth <phil@netlify.com>
2020-02-29 19:16:59 +0000

    localisation

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,12 +1,27 @@
 const axios = require("axios");
+const countries = require("./countries.json");
+
 require('dotenv').config();
 
-module.exports = async function() {
+async function getNews(country) {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-    return response.data;
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    return {
+     "country": country,
+     "articles": response.data.articles
+    }
   } catch (error) {
     console.error(error);
   }
+}
+
+
+module.exports = async function() {
+
+    var newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then( newsObjects => {
+      console.log('newsObjects:', newsObjects);
+      return [].concat.apply([],newsObjects);
+    });
 
 };
