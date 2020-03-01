4a9dd7209e705056b4cafc7dc16dd99e389f1c58
Phil Hawksworth <phil@netlify.com>
2020-03-01 18:14:20 +0000

    all pages

--- src/_data/news.js
+++ src/_data/news.js
@@ -20,7 +20,6 @@ module.exports = async function() {
 
     var newsPromises = countries.map(getNews);
     return Promise.all(newsPromises).then( newsObjects => {
-      console.log('newsObjects:', newsObjects);
       return [].concat.apply([],newsObjects);
     });
 
